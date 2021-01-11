16f7c636539a4ac3a89749860a0cbd6b8f1b81af
Debdut Goswami <debdutgoswami@gmail.com>
2021-01-11 13:03:52 +0530

    added Room model to admin portal
    Closes #7

---
+++ src/backend/lenzo/canvas/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import *
+
+
+class RoomAdmin(admin.ModelAdmin):
+    """
+    Room Interface in Admin Page
+    """
+
+    list_display = [
+        "id",
+        "name",
+        "host",
+        "created_at",
+        "is_live",
+        "duration",
+        "start_time",
+        "end_time",
+    ]
+
+
+admin.site.register(Room, RoomAdmin)
