d25a3747e0e9421ecd5c92c3f078dd8560da02eb
Debdut Goswami <debdutgoswami@gmail.com>
2021-06-22 17:13:57 +0530

    FileField => ImageField

--- src/backend/canvas/models/photos.py
+++ src/backend/canvas/models/photos.py
@@ -5,4 +5,4 @@ from canvas.models.room import Room
 
 class RoomPhotos(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="photos")
-    image = models.FileField(upload_to="photos")
+    image = models.ImageField(upload_to="photos")
